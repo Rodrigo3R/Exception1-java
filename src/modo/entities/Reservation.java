abbf4351e75052b49c0767158b5a98d3394f7150
Rodrigo3R <rodrigo3r7@hotmail.com>
2020-05-29 14:54:04 -0300

    Good solution

--- src/modo/entities/Reservation.java
+++ src/modo/entities/Reservation.java
@@ -4,19 +4,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
-	
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	public Reservation() {
 	}
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 
+		if (!checkOut.after(checkIn)) {
+
+			throw new DomainException("Check-Out date must be after check-in date.");
+
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -38,48 +45,45 @@ public class Reservation {
 		return checkOut;
 	}
 
-	public long  duration() {
-		
-		
+	public long duration() {
+
 		// calculando a diferença do tempo e colocando em dias.
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-		
+
 	}
-	
-	public String updateDates(Date checkIn, Date checkOut) {
-		
+
+	public void updateDates(Date checkIn, Date checkOut) {
+
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			
-			return "Reservation dates for update must be futures date.";
-			
-		} 
+
+			throw new DomainException("Reservation dates for update must be futures date.");
+
+		}
 		if (!checkOut.after(checkIn)) {
-			
-			return "Check-Out date must be after check-in date.";
-			
+
+			throw new DomainException("Check-Out date must be after check-in date.");
+
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		
-		return null;
+
 	}
-	
+
 	@Override
 	public String toString() {
-		
+
 		StringBuffer sb = new StringBuffer();
-		
+
 		sb.append("Room: ");
 		sb.append(roomNumber + "\ncheckIn: ");
 		sb.append(sdf.format(checkIn) + "\n");
 		sb.append("ckeckOut: ");
 		sb.append(sdf.format(checkOut) + "\n");
 		sb.append(duration() + " nights");
-		
+
 		return sb.toString();
 	}
-	
-}
 
+}
